31e7cfad86c7170c24cb8e14772c62080fc2d01a
Hoang3409 <nvh.02021995@gmail.com>
2022-12-23 17:28:37 +0700

    Add entity database "Eshop" , add controller "AdminCategory"

---
+++ Controllers/Category/AdminCategoryController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Eshop.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        // GET: AdminCategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
