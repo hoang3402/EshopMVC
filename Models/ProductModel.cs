92423f11be60bd4a536de60d1551aaffb76a5bff
Hoang3409 <nvh.02021995@gmail.com>
2022-12-23 17:51:28 +0700

    Implement Product controller, edit view for AdminCategory, AdminProduct

---
+++ Models/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿using Eshop.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MVC.Models
+{
+    public class ProductModel
+    {
+        static EShopEntities database = new EShopEntities();
+
+        public static Product Find(int id)
+        {
+            return database.Products.Find(id);
+        }
+
+        public static List<Product> GetProducts()
+        {
+            return database.Products.ToList();
+        }
+    }
+}
